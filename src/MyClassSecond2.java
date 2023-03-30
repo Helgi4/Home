03136d60bd651c29ec432a547f1e30d123cb4998
Oleh Moroz <oleh.moroz01@gmail.com>
2023-03-30 17:01:20 +0300

    test commit

--- src/MyClassSecond2.java
+++ src/MyClassSecond2.java
@@ -1,9 +1,15 @@
 public class MyClassSecond2 {
+    public static void main(String[] args) {
+        String word1 = "Oleh";
+        String word2 = "Moroz";
+        sumOfLengths(word1, word2);
+    }
+
     public static void sumOfLengths(String word1, String word2) {
         int sum = word1.length() + word2.length();
         System.out.println("The sum of the lengths of " + word1 + " and " + word2 + " is " + sum);
     }
-
 }
+
 
 
